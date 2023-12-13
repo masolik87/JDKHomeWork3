344b536efe1ab1909868b76b35517254b6125c7e
dimon <dimon20.07@mail.ru>
2023-12-13 18:26:22 +0300

    .\Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Calculator {
+    public static <T extends Number, U extends Number> double sum(T num1, U num2) {
+
+        return num1.doubleValue() + num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double multiply(T num1, U num2) {
+        return num1.doubleValue() * num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double divide(T num1, U num2) {
+        return num1.doubleValue() / num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double subtract(T num1, U num2) {
+        return num1.doubleValue() - num2.doubleValue();
+    }
+}
